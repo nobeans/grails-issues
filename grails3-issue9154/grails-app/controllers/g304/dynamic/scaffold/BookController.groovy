339fb7534c05ac9686ab14a9cb4257c9fc0d64cd
Yasuharu Nakano <ynak@jggug.org>
2015-07-31 17:03:36 +0900

    Add the example for https://github.com/grails/grails-core/issues/9154

---
+++ grails3-issue9154/grails-app/controllers/g304/dynamic/scaffold/BookController.groovy
@@ -0,0 +1,7 @@
+package g304.dynamic.scaffold
+
+class BookController {
+
+    static scaffold = Book
+
+}
---
+++ grails3-issue9154/grails-app/domain/g304/dynamic/scaffold/Book.groovy
@@ -0,0 +1,10 @@
+package g304.dynamic.scaffold
+
+class Book {
+
+    String title
+
+    static constraints = {
+        title maxSize: 10
+    }
+}
